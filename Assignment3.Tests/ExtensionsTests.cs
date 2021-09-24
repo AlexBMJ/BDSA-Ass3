5f96535934bf0759a2b070078ab3e2c126e8ae78
Alex <AlexBMJ@users.noreply.github.com>
2021-09-24 13:28:57 +0200

    Implement and test Flatten as Extension Method

--- Assignment3.Tests/ExtensionsTests.cs
+++ Assignment3.Tests/ExtensionsTests.cs
@@ -1,14 +1,21 @@
 using System;
+using System.Collections.Generic;
 using Xunit;
 
-namespace BDSA2020.Assignment02.Tests
-{
-    public class ExtensionsTests
-    {
+namespace Assignment3.Tests {
+    public class ExtensionsTests {
         [Fact]
-        public void Test1()
-        {
-
+        public void Flatten_Given_Nested_Array_1through6_returns_Single_Array_1through6() {
+            IEnumerable<int>[] list = {new[] {1, 2}, new[] {3, 4}, new[] {5, 6}};
+            var result = list.Flatten();
+            Assert.Equal(new[] {1, 2, 3, 4, 5, 6}, result);
+        }
+        
+        [Fact]
+        public void Flatten_GivenNullList_SkipNullList() {
+            IEnumerable<int>[] list = {new[] {1, 2}, new[] {3, 4}, null, new[] {5, 6}};
+            var result = list.Flatten();
+            Assert.Equal(new[] {1, 2, 3, 4, 5, 6}, result);
         }
     }
 }
--- Assignment3/Extensions.cs
+++ Assignment3/Extensions.cs
@@ -1,8 +1,11 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace BDSA2020.Assignment02
+namespace Assignment3
 {
-    public static class Extensions
-    {
+    public static class Extensions {
+        public static IEnumerable<T> Flatten<T>(this IEnumerable<IEnumerable<T>> items) =>
+            items.SelectMany(itemList => itemList ??new List<T>());
     }
 }
