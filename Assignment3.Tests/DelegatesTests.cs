038f38cc925314bea05b745d60d3cc67708ecfb2
Deniz Isik <deniz_4600@live.dk>
2021-09-26 23:52:05 +0200

    added anonymous method

--- Assignment3.Tests/DelegatesTests.cs
+++ Assignment3.Tests/DelegatesTests.cs
@@ -6,9 +6,22 @@ namespace Assignment3.Tests
     public class DelegatesTests
     {
         [Fact]
-        public void Test1()
+        public void Func_returns_12_when_multiplying_3_by_4()
         {
+            Assert.Equal(12, Extensions.product(3,4));
+           
+        }
 
+        [Fact]
+        public void Func_returns_12_when_multiplying_3_y_4()
+        {
+            Assert.Equal("olleh", Extensions.reverse("hello"));
+        }
+
+        [Fact]
+        public void Func_returns_12_when_multiplyg_3_y_4()
+        {
+            Assert.Equal(true, Extensions.isNumericallyEqual(42, "0042")); 
         }
     }
 }
