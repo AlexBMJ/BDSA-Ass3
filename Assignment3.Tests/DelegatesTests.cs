68819d9e7bd90903a837fa97ac78765c8a604677
Deniz Isik <deniz_4600@live.dk>
2021-09-27 14:46:59 +0200

    change method names

--- Assignment3.Tests/DelegatesTests.cs
+++ Assignment3.Tests/DelegatesTests.cs
@@ -6,20 +6,20 @@ namespace Assignment3.Tests
     public class DelegatesTests
     {
         [Fact]
-        public void Func_returns_12_when_multiplying_3_by_4()
+        public void Func_Product_returns_12_when_multiplying_3_by_4()
         {
             Assert.Equal(12, Extensions.Product(3,4));
            
         }
 
         [Fact]
-        public void reverse()
+        public void Func_Revers_returns_olleh_when_given_hello()
         {
             Assert.Equal("olleh", Extensions.Reverse("hello"));
         }
 
         [Fact]
-        public void Func_returns_true_for_string0042_and_integer_42()
+        public void Func_IsNumericallyEqual_returns_true_for_string0042_and_integer_42()
         {
             Assert.True(Extensions.IsNumericallyEqual(42, "0042")); 
         }
--- Assignment3/Extensions.cs
+++ Assignment3/Extensions.cs
@@ -1,11 +1,48 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Assignment3
 {
     public static class Extensions {
         public static IEnumerable<T> Flatten<T>(this IEnumerable<IEnumerable<T>> items) =>
             items.SelectMany(itemList => itemList ??new List<T>());
+
+    
+        public static bool IsSecure(this Uri link)
+        {
+            return link.AbsoluteUri.StartsWith("http") && (link.AbsoluteUri.EndsWith(".com/") || link.AbsoluteUri.EndsWith(".dk/") || link.AbsoluteUri.EndsWith(".org/"));
+        }
+
+        public static int WordCount(this string input){
+            Regex regex = new Regex("[a-zA-Z]");
+            var matches = regex.Matches(input);
+            int counter = 0;
+            foreach (var match in matches){
+                counter++;
+            }
+            return counter;
+        }
+
+        public static string ReverseString(string input){
+            char[] arr = input.ToCharArray();
+            Array.Reverse(arr);
+            return new string(arr);
+        }
+
+        public static bool NumberIsEqual(int number, string input){
+            return Int32.Parse(input).Equals(number);
+        }
+
+        //1
+        public static Func<string, string> Reverse = ReverseString;
+
+        // 2.
+         public static Func<int, int, int> Product = (a, b) => a * b;
+
+         //3
+         public static Func<int, string, bool> IsNumericallyEqual = NumberIsEqual;
     }
+    
 }
