f0f1108cf2a735b5cd8fc2078ca5eff3c1733588
Alex <AlexBMJ@users.noreply.github.com>
2021-09-27 23:07:57 +0200

    Fix and implement Wizard queries

--- Assignment3.Tests/QueriesTests.cs
+++ Assignment3.Tests/QueriesTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace Assignment3.Tests
@@ -6,12 +7,24 @@ namespace Assignment3.Tests
     public class QueriesTests
     {
         [Fact]
-        public void Query_GetWizardNames_returns_only_names_of_wizards_invented_by_Rowling()
+        public void GetWizardNamesByExtension_returns_only_names_of_wizards_invented_by_Rowling()
         {
             string[] wizardNames = {"Lord Voldemort" , "Albus Dumbledore"};
-
             Assert.Equal(wizardNames, Queries.GetWizardNamesByExtension());
-
+        }
+        
+        [Fact]
+        public void GetWizardNamesByLINQ_returns_only_names_of_wizards_invented_by_Rowling()
+        {
+            (string,int?)[] wizardNames = {("Lord Voldemort",2001) , ("Albus Dumbledore",2001)};
+            Assert.Equal(wizardNames, Queries.GetWizardsFromHarryPotterByExtension());
+        }
+        
+        [Fact]
+        public void GetWizardsFromHarryPotterByExtension_ReturnsNameAndYear()
+        {
+            (string,int?)[] wizardNames = {("Lord Voldemort",2001) , ("Albus Dumbledore",2001)};
+            Assert.Equal(wizardNames, Queries.GetWizardsFromHarryPotterByLINQ());
         }
     }
 }
--- Assignment3/Queries.cs
+++ Assignment3/Queries.cs
@@ -7,35 +7,20 @@ namespace Assignment3
     public class Queries
     {
 
-        public static IEnumerable<string> GetWizardNamesByExtension()
-        {
-            IEnumerable<string> WizardNames = Wizard.Wizards.Value.Where(w => w.Creator.Contains("Rowling")).Select(c => c.Name);
-             return WizardNames;
-        }
+        public static IEnumerable<string> GetWizardNamesByExtension() => 
+            Wizard.Wizards.Value.Where(w => w.Creator.Contains("Rowling")).Select(c => c.Name);
 
-        public static IEnumerable<string> GetWizardNamesByLINQ()
-        {
-            IEnumerable<string> WizardNames = (from wizard in Wizard.Wizards.Value
-                                                where wizard.Creator.Contains("Rowling")
-                                                select wizard.Name);
-             return WizardNames;
-        }
+        public static IEnumerable<string> GetWizardNamesByLINQ() => 
+            from wizard in Wizard.Wizards.Value
+            where wizard.Creator.Contains("Rowling")
+            select wizard.Name;
 
-        public static IEnumerable<(string,int?)> GetWizardsFromHarryPotterByExtension()
-        {
-            var harryPotter = Wizard.Wizards.Value.Where(w => w.Medium.Contains("Harry Potter"))
-                                            .Select(c => (c.Name, c.Year));
-            return harryPotter;
-        }
+        public static IEnumerable<(string,int?)> GetWizardsFromHarryPotterByExtension() => 
+            Wizard.Wizards.Value.Where(w => w.Medium.Contains("Harry Potter")).Select(c => (c.Name, c.Year));
 
-        public static IEnumerable<(string, int?)> GetWizardsFromHarryPotterByLINQ()
-        {
-            var harryPotter = from wizard in Wizard.Wizards.Value
-                                where wizard.Medium.Contains("Harry Potter")
-                                select (wizard.Name, wizard.Year) ;
-            return harryPotter;
-        }
-       
-
+        public static IEnumerable<(string, int?)> GetWizardsFromHarryPotterByLINQ() => 
+            from wizard in Wizard.Wizards.Value
+            where wizard.Medium.Contains("Harry Potter")
+            select (wizard.Name, wizard.Year);
     }
 }
