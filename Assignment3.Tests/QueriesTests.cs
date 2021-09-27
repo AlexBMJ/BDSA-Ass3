811f3e03f026a6916da65409c76403ed0fba4b05
Deniz Isik <deniz_4600@live.dk>
2021-09-27 16:35:41 +0200

    added LINQ and Extension methods

--- Assignment3.Tests/QueriesTests.cs
+++ Assignment3.Tests/QueriesTests.cs
@@ -6,8 +6,11 @@ namespace Assignment3.Tests
     public class QueriesTests
     {
         [Fact]
-        public void Test1()
+        public void Query_GetWizardNames_returns_only_names_of_wizards_invented_by_Rowling()
         {
+            string[] wizardNames = {"Lord Voldemort" , "Albus Dumbledore"};
+
+            Assert.Equal(wizardNames, Queries.GetWizardNames();
 
         }
     }
--- Assignment3/Queries.cs
+++ Assignment3/Queries.cs
@@ -1,6 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Assignment3
 {
     public class Queries
     {
+
+        public static IEnumerable<string> GetWizardNamesByExtension()
+        {
+            IEnumerable<string> WizardNames = Wizard.Wizards.Where(w => w.Creator.Contains("Rowling")).Select(c => c.Name);
+             return WizardNames;
+        }
+
+        public static IEnumerable<string> GetWizardNamesByLINQ()
+        {
+            IEnumerable<string> WizardNames = (from wizard in Wizard.Wizards
+                                                where wizard.Creator.Contains("Rowling")
+                                                select wizard.Name);
+             return WizardNames;
+        }
+
+        public static IEnumerable<string> GetWizardsFromHarryPotterByExtension()
+        {
+            var harryPotter = Wizard.Wizards.Where(w => w.Medium.Contains("Harry Potter"))
+                                            .Select(c => new {Name = c.Name,
+                                                              Year = c.Year});
+        }
+
+        public static IEnumerable<string> GetWizardsFromHarryPotterByLINQ()
+        {
+            var harryPotter = from wizard in Wizard.Wizards
+                                where wizard.Medium.Contains("Harry Potter")
+                                select new {Name = wizard.Name,
+                                            Year = wizard.Year};
+        }
+       
+
     }
 }
